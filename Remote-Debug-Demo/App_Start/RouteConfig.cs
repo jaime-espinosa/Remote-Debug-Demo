f53bd8936a2b3cd77c02763e23b2e39252eff51d
Jaime Espinosa <jaime@microsoft.com>
2015-01-14 21:56:49 -0800

    Initial comit

---
+++ Remote-Debug-Demo/App_Start/RouteConfig.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Routing;
+using Microsoft.AspNet.FriendlyUrls;
+
+namespace Remote_Debug_Demo
+{
+    public static class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.EnableFriendlyUrls();
+        }
+    }
+}
